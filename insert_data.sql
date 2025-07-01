2c4cb20f972d4375177d9cdf67c52e3d933161e3
Himanshu <Himanshumanoriwal769@outlook.com>
2025-07-01 13:04:38 +1000

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -1,5 +1,5 @@
 
--- Insert data using PostgreSQL COPY
+-- Insert data using PostgreSQL
 COPY hospital_beds(country, state, county, latitude, longitude, bed_type, measure,
                    beds_per_1000, population, year, source, source_url)
 FROM 'data/hospital_beds_USA_v1.csv'
